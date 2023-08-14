fa29982afd2a57a5476e89a0579a475d1c5ed6ef
Messef <Akkubear@gmail.com>
2023-08-14 12:08:41 -0400

    made py folder

--- C++_Programs/main.cpp
+++ C++_Programs/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <string>
+
 using namespace std; 
 
+
 int x = 0;
+string y = "";
 int main() {
     cout<<x << endl; 
     cout<<"Hello!" << endl;
+    cout<<"Type in a number: ";
+    cin>>y;
+    cout<<y;
+    cout<<endl;
+
 }
