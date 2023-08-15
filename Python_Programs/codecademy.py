06e3b474595d3b15dac75c3556810483914620a7
Messef <Akkubear@gmail.com>
2023-08-15 12:17:25 -0400

    Codecademy day 2 p1

--- Python_Programs/codecademy.py
+++ Python_Programs/codecademy.py
@@ -1,9 +1,29 @@
-from random import randint
-# Define the release and runtime integer variables below:
-release_year = 0
-runtime = 0
+class realStudent:
+    def __init__(self, name = "", age = 0, grades = []):
+        self.name = name
+        self.age = age
+        self.grades = []
+    
+    def addG (self, grade: int):
+        self.grades.append(grade)
+        print(self.grades)
 
 
-# Define the rating_out_of_10 float variable below: 
-rating_out_of_10 = randint(1, 10)
-print(rating_out_of_10)
+def createStudent(name, age, grades=[]):
+    return {
+        'name': name,
+        'age': age,
+        'grades': grades
+    }
+chrisley = createStudent('Chrisley', 15)
+dallas = createStudent('Dallas', 16)
+c2 = realStudent("c2", 1)
+d2 = realStudent("d2",2)
+def addGrade(student, grade):
+    student['grades'].append(grade)
+    # To help visualize the grades we have added a print statement
+    print(student['grades'])
+addGrade(chrisley, 90)
+addGrade(dallas, 100)
+c2.addG(90)
+d2.addG(100)
