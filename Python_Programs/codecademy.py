f0fb713dcd904ccada31d3b54e084ea26fd570d2
Messef <Akkubear@gmail.com>
2023-08-15 11:48:33 -0400

    Codecademy Beginner Day 1

---
+++ Python_Programs/codecademy.py
@@ -0,0 +1,9 @@
+from random import randint
+# Define the release and runtime integer variables below:
+release_year = 0
+runtime = 0
+
+
+# Define the rating_out_of_10 float variable below: 
+rating_out_of_10 = randint(1, 10)
+print(rating_out_of_10)
