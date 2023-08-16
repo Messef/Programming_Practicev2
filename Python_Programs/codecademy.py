9b3d237e4f30e22b20fcaf313207718174798a72
Messef <Akkubear@gmail.com>
2023-08-16 11:48:35 -0400

    Update codecademy.py

--- Python_Programs/codecademy.py
+++ Python_Programs/codecademy.py
@@ -1,29 +1,24 @@
-class realStudent:
-    def __init__(self, name = "", age = 0, grades = []):
-        self.name = name
-        self.age = age
-        self.grades = []
-    
-    def addG (self, grade: int):
-        self.grades.append(grade)
-        print(self.grades)
 
 
-def createStudent(name, age, grades=[]):
-    return {
-        'name': name,
-        'age': age,
-        'grades': grades
-    }
-chrisley = createStudent('Chrisley', 15)
-dallas = createStudent('Dallas', 16)
-c2 = realStudent("c2", 1)
-d2 = realStudent("d2",2)
+def createStudent(name, age, grades=None):
+  if grades is None:
+    grades = []
+  return {
+    'name': name,
+    'age': age,
+    'grades': grades
+  }
+ 
 def addGrade(student, grade):
     student['grades'].append(grade)
     # To help visualize the grades we have added a print statement
     print(student['grades'])
-addGrade(chrisley, 90)
+ 
+chrisley = createStudent('Chrisley', 15)
+dallas = createStudent('Dallas', 16)
+def addGrade(student, grade):
+    student['grades'].append(grade)
+    # To help visualize the grades we have added a print statement
+    print(student['grades'])
+addGrade(chrisley, [90,89, 78, 100])
 addGrade(dallas, 100)
-c2.addG(90)
-d2.addG(100)
