9b00f0d7a30afb5f4c1830f947e0ca9fdf50a9fb
Messef <Akkubear@gmail.com>
2023-08-18 09:44:52 -0400

    Update codecademy.py

--- Python_Programs/codecademy.py
+++ Python_Programs/codecademy.py
@@ -1,41 +1,19 @@
-'''
-For an upcoming holiday, Jiho plans on making a prix fixe menu for the restaurant. Customers at the restaurant will be able to choose the following:
-
-1 Appetizer
-2 Entrees
-1 Side dish
-2 Scoops of different ice cream flavors for dessert.
-
-To accomplish all these choices, we are going to utilize the different types of 
-arguments that we have learned so far. Now that we’ve set up our 
-goals, hit “Run” to move on to the next step.'''
-tables = {
-  1: {
-    'name': 'Chioma',
-    'vip_status': False,
-    'order': {
-      'drinks': 'Orange Juice, Apple Juice',
-      'food_items': 'Pancakes'
-    }
-  },
-  2: {
-    'name': 'Chioma',
-    'vip_status': False,
-    'order': {
-      'drinks': 'Orange Juice, Apple Juice',
-      'food_items': 'Pancakes'
-  }
-  },
-  3: {},
-  4: {},
-  5: {},
-  6: {},
-  7: {},
-}
-
-def single_prixe_fixe_order(table_number,appetizer, *entrees, side_dish, **scoops):
-    scoops=scoops.get('scoops')
-    tables[table_number]['order']['food_items']=appetizer, entrees, side_dish,scoops
-    print(tables)
-single_prixe_fixe_order(2,'garlic bread', 'Omelette', 'Steak', side_dish='Fires', scoops=['Chocolate', 'Butterscotch'])
-
+nums=[2,7,11,15]
+target=9
+class Solution():
+    def twoSum(test, nums, target):
+        test.nums=[]
+        test.target=0
+    def findIndicies(nums, target):
+        x=0
+        y=0
+        while x<(len(nums)):
+            while y<(len(nums)):
+                if nums[x]+nums[y]==target:
+                    return x, y
+                    break
+                else:
+                    y+=1
+            x+=1
+test=Solution()
+Solution.findIndicies(nums, target)
