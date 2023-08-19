28fcbdb61ca01df902691503243a9a56eb9fb932
Messef <Akkubear@gmail.com>
2023-08-19 17:23:35 -0400

    Bhaiya's coding solution

--- Python_Programs/codecademy.py
+++ Python_Programs/codecademy.py
@@ -1,19 +1,27 @@
-nums=[2,7,11,15]
-target=9
-class Solution():
-    def twoSum(test, nums, target):
-        test.nums=[]
-        test.target=0
-    def findIndicies(nums, target):
-        x=0
-        y=0
-        while x<(len(nums)):
-            while y<(len(nums)):
-                if nums[x]+nums[y]==target:
-                    return x, y
-                    break
-                else:
-                    y+=1
-            x+=1
-test=Solution()
-Solution.findIndicies(nums, target)
+word="chess"
+letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+def find_unique_letters(word):
+    #Fast way (doesn't work if word has non-alphabetic characters)
+    '''
+    wordSet = set(word)
+    return len(wordSet)
+    '''
+    uniqueSet = set(letters)
+    count = 0
+    for char in word:
+        if char in uniqueSet:
+            count+=1
+            uniqueSet.remove(char)
+
+    return count
+def unique_english_letters(word):
+  uniques = 0
+  for letter in letters:
+    if letter in word:
+      uniques += 1
+  return uniques
+
+
+
+
+print(find_unique_letters(word))
---
+++ Python_Programs/test.py
@@ -0,0 +1,4 @@
+txt="Hello"
+x=0
+txt=txt.replace(txt[1:2], "")
+print( txt)
