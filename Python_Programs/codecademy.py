9b618cb6d4084c71234c518823431be3b428d2da
Messef <Akkubear@gmail.com>
2023-08-20 17:21:46 -0400

    Solution to the TwoSum Problem

--- Python_Programs/codecademy.py
+++ Python_Programs/codecademy.py
@@ -1,45 +1,13 @@
-number=int(input("Integer number back to binary: "))
-binary=[128, 64, 32, 16, 8, 4, 2, 1]
-nums=0
-convert=0
-def can_convert(number):
-  global binary
-  global nums, convert
-  convert=2
-  print(type(binary))
-  for i in binary:
-    nums+=i
-  while number>nums:
-    new_binary=binary[0]*convert
-    binary.append(new_binary)
-    convert*=2
-    nums=0
-    for i in binary:
-      nums+=i
-  else:
-    binary.sort()
-    binary.reverse()
-    return binary, nums, convert
-
-def convert_to_Binary(number):
-  global binary
-  can_convert(number)
-  if number>255:
-    can_convert(number)
-  else: 
-    pass
-  binary_number=""
-  binary_int=0
-  for x in binary:
-    if number>x and number>=binary_int+x:
-      binary_number+="1"
-      binary_int+=x
-    else:
-      binary_number+="0"
-  return binary_number, binary
-while type(number)==int:
-  print(convert_to_Binary(number), convert)
-  binary=[128, 64, 32, 16, 8, 4, 2, 1]
-  number=int(input("Integer number back to binary: "))
-else:
-  quit()
+'''
+Solution to the two sum problem
+'''
+nums=[2, 6, 7, 11, 10]
+def twoSum(nums, target: int):
+        hashmap = {}
+        for i in range(len(nums)):
+            hashmap[nums[i]] = i
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return [i, hashmap[complement], hashmap] 
+print(twoSum(nums, 9))
