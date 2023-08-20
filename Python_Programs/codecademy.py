50608fab5d3b8da3197b4c3996a1b568f2c5c21b
Messef <Akkubear@gmail.com>
2023-08-20 15:05:46 -0400

    Convert int to Binary

--- Python_Programs/codecademy.py
+++ Python_Programs/codecademy.py
@@ -1,14 +1,45 @@
-def count_char_x(word, x):
-  counter=0
-  letter=0
-  while letter<len(word):
-    if word[letter]==x:
-      counter+=1
-      letter+=1
-    else: 
-      letter+=1
-      continue
-  else: 
-    return counter
+number=int(input("Integer number back to binary: "))
+binary=[128, 64, 32, 16, 8, 4, 2, 1]
+nums=0
+convert=0
+def can_convert(number):
+  global binary
+  global nums, convert
+  convert=2
+  print(type(binary))
+  for i in binary:
+    nums+=i
+  while number>nums:
+    new_binary=binary[0]*convert
+    binary.append(new_binary)
+    convert*=2
+    nums=0
+    for i in binary:
+      nums+=i
+  else:
+    binary.sort()
+    binary.reverse()
+    return binary, nums, convert
 
-print(count_char_x("apple", "p"))
+def convert_to_Binary(number):
+  global binary
+  can_convert(number)
+  if number>255:
+    can_convert(number)
+  else: 
+    pass
+  binary_number=""
+  binary_int=0
+  for x in binary:
+    if number>x and number>=binary_int+x:
+      binary_number+="1"
+      binary_int+=x
+    else:
+      binary_number+="0"
+  return binary_number, binary
+while type(number)==int:
+  print(convert_to_Binary(number), convert)
+  binary=[128, 64, 32, 16, 8, 4, 2, 1]
+  number=int(input("Integer number back to binary: "))
+else:
+  quit()
--- Python_Programs/test.py
+++ Python_Programs/test.py
@@ -1,4 +1,25 @@
-txt="Hello"
-x=0
-txt=txt.replace(txt[x:x+1], "")
-print( txt)
+binary=[128, 64, 32, 16, 8, 4, 2, 1]
+nums=0
+def can_convert(number):
+  global binary
+  global nums
+  convert=2
+  print(type(binary))
+  for i in binary:
+    nums+=i
+  while number>nums:
+    print(type(binary))
+    new_binary=binary[0]*convert
+    print(type(binary))
+    binary.append(new_binary)
+    print(type(binary), binary, nums)
+    print(type(binary), binary,nums)
+    convert+=2
+    nums=0
+    for i in binary:
+      nums+=i
+  else:
+    binary.sort()
+    binary.reverse()
+    return binary, nums
+print(can_convert(1000))
