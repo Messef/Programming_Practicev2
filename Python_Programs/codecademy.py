fea26b714256cc561979ffd1396ea52557c59784
Messef <Akkubear@gmail.com>
2023-08-24 12:39:54 -0400

    LeetCode Problem 9
    Finding a needle in the haystack

--- Python_Programs/codecademy.py
+++ Python_Programs/codecademy.py
@@ -1,41 +1,7 @@
-def intToRoman(num: int) -> str:
-    hashmap={
-            1000:'M',
-            900: 'CM', 
-            500: 'D',
-            400:'CD',
-            100:'C',
-            90:'XC',
-            50:'L',
-            40:'XL',
-            10: 'X',
-            9: 'IX',
-            5: 'V',
-            4:'IV',
-            1:'I',
-            0:''
-            
-        }
-    new_num = [int(num) for num in str(num)]
-    ans=""
-    x=0
-    while x<len(new_num):
-       math=len(new_num)-(x+1)
-       new_num[x]=new_num[x]*10**math
-       x+=1
-    for y in new_num:
-       if y in hashmap:
-        ans+=hashmap[y]
-       else: 
-        counter=0
-        for x in hashmap.keys():
-          if x<y:
-            while (counter+x)<=y and x!=0:
-             print(x, counter, ans)
-             ans+=hashmap[x]
-             counter+=x
-            else:
-              continue
-           
-    return new_num, ans
-print(intToRoman(112))
+import time
+start_time = time.time()
+def strStr(haystack: str, needle: str) -> int:
+      
+      return haystack.find(needle)
+print(strStr("sadbutsad", "bu"))
+print("--- %s seconds ---" % (time.time() - start_time))
--- Python_Programs/test.py
+++ Python_Programs/test.py
@@ -1,22 +1,8 @@
-def intToRoman(num: int) -> str:
-        roman_numerals={
-            1000: "M",
-            900: "CM",
-            500: "D",
-            400: "CD",
-            100: "C",
-            90: "XC",
-            50: "L",
-            40: "XL",
-            10: "X",
-            9: "IX",
-            5: "V",
-            4: "IV",
-            1: "I"
-        }
-        roman=''
-        for value,symbol in roman_numerals.items():
-            while value<=num:
-                roman+=symbol
-                num=num-value
-        return roman
+a=["a", 'b', 'c']
+counter=0
+for i in enumerate(a):
+    print(i)
+    if i=='a':
+        print('nice')
+    counter+=1
+print(counter)
