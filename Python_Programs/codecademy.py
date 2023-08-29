b06d962ab467f338ac0228e30d273e949fa636fa
Messef <Akkubear@gmail.com>
2023-08-29 13:36:25 -0400

    Length of Longest Substring
    Use pointers to solve.
    Proper solution on test.py
    LeetCodeProblem 12

--- Python_Programs/codecademy.py
+++ Python_Programs/codecademy.py
@@ -1,14 +1,19 @@
-'''Example 1:
-
-Input: n = 00000010100101000001111010011100
-Output:    964176192 (00111001011110000010100101000000)
-Explanation: The input binary string 00000010100101000001111010011100 represents the unsigned integer 
-43261596, so return 964176192 which its binary representation is 00111001011110000010100101000000.'''
-def reverseBits(n: str) -> int:
-    ans=0
-    for x, y in enumerate(n[::-1]):
-     if y=="1":
-      mod=2**(len(n)-x-1)
-      ans+=mod
-    return ans, n
-print(reverseBits("00000010100101000001111010011100"))
+def lengthOfLongestSubstring( s: str) -> int:
+        seen=[]
+        values=[0]
+        counter=0
+        for  y in s:
+            if y not in seen:
+                seen.append(y)
+                counter+=1
+            else:
+                values.append(counter)
+                seen=seen[seen.index(y):]
+                seen.remove(y)
+                seen.append(y)
+                counter=len(seen)
+        values.append(counter)
+        return max(values)
+            
+            
+print(lengthOfLongestSubstring("aabaab!bb"))
--- Python_Programs/test.py
+++ Python_Programs/test.py
@@ -1,9 +1,15 @@
-def reverseBits(n: int) -> int:
-        ans=0
-        n=[int(n) for n in str(n)]
-        for x, y in enumerate(n[::-1]):
-          if y==1:
-            mod=2**(len(n)-x-1)
-            ans+=mod
-        return ans, n
-print(reverseBits(0b10000010100101000001111010011100))
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        seen = {}
+        l = 0
+        output = 0
+        for r, x in enumerate(s):
+            if x not in seen:
+                output = max(output,r-l+1)
+            else:
+                if seen[x] < l: #Find out why lines 10 and 11 are here and what they do
+                    output = max(output,r-l+1)
+                else:
+                    l = seen[x] + 1
+            seen[x] = r
+        return output
