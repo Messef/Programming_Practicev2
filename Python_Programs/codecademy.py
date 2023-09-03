6cfa4f75d54d711ac05c37725fb4b6be7f08185a
Messef <Akkubear@gmail.com>
2023-09-03 11:52:38 -0400

    Binary search proper
    LeetCode Problem 15
    test.py has a method of b. search
    codecademy.py has the way to do it by hand

--- Python_Programs/codecademy.py
+++ Python_Programs/codecademy.py
@@ -1,15 +1,12 @@
-def maxArea(height):
-    dist=1
-    output=0
-    left=0
-    right=1
-    while right<len(height):
-        nums=min(height[left], height[right])
-        nums*=dist
-        output=max(nums, output)
-        if height[left]<height[right]:
-            left=right
-            dist=0
-        dist+=1; right+=1
-    return output
-print(maxArea([1,2,1]))
+def searchInsert( nums, target: int) -> int:
+        l=0
+        r=len(nums)
+        while l < r:
+            mid=(l+r) // 2
+            if nums[mid]<target: 
+                  l=mid+1
+            else: 
+                  r=mid
+        return l
+                  
+print(searchInsert([1,3,5,6], 7))
--- Python_Programs/test.py
+++ Python_Programs/test.py
@@ -1,14 +1,5 @@
+import bisect
+
 class Solution:
-    def maxArea(height) -> int:
-        ma= 0 #max area
-        st= 0 #start
-        en= len(height)-1 #end
-        while st< en: #while these two pointers don't interesect
-            an= (min(height[st], height[en]))* (en- st) #current area
-            if an> ma: #if the max area is less than the current area update the variable
-                ma= an
-            if height[st] < height[en]: #if the left pointer's height is less than the right pointer decrement the left one. This works to find a higher height
-                st+=1
-            else: #move the right pointer closer in
-                en-=1
-        return ma #answer
+    def searchInsert(self, nums, target: int) -> int:
+        return bisect.bisect_left(nums, target)
