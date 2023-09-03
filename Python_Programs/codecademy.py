f09c1599b6ebed3b3c7e5a567f1ef1a41b418869
Messef <Akkubear@gmail.com>
2023-09-03 18:17:17 -0400

    Binary Search Properly Done v2
    Leetcode Problem 16

--- Python_Programs/codecademy.py
+++ Python_Programs/codecademy.py
@@ -1,12 +1,15 @@
-def searchInsert( nums, target: int) -> int:
-        l=0
-        r=len(nums)
-        while l < r:
-            mid=(l+r) // 2
-            if nums[mid]<target: 
-                  l=mid+1
-            else: 
-                  r=mid
-        return l
-                  
-print(searchInsert([1,3,5,6], 7))
+def mySqrt(x: int) -> int:
+    l=1
+    r= x // 2
+    while l<=r:
+        mid = (l+r) // 2
+        print(l, mid, r)
+        if mid**2>x:
+            r=mid-1
+        elif mid**2<x:
+            l=mid+1
+        else: 
+            return mid
+    return r
+
+print(mySqrt(8))
