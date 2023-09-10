9832d265aa8db6faf647bef648f4ea121ea77747
Messef <Akkubear@gmail.com>
2023-09-09 21:18:56 -0400

    Launched Java and tried to get c++ running

--- C++_Programs/main.cpp
+++ C++_Programs/main.cpp
@@ -7,11 +7,6 @@ using namespace std;
 int x = 0;
 string y = "";
 int main() {
-    cout<<x << endl; 
-    cout<<"Hello!" << endl;
-    cout<<"Type in a number: ";
-    cin>>y;
-    cout<<y;
-    cout<<endl;
-
+    cout<<"Hello World!";
+    return 0;
 }
--- Python_Programs/codecademy.py
+++ Python_Programs/codecademy.py
@@ -1,12 +1,1 @@
-def divide(dividend: int, divisor: int) -> int:
-    counter = 0
-    store_dividend = dividend
-    store_divisor = divisor
-    dividend = abs(dividend)
-    divisor = abs(divisor)
-    while dividend >= divisor: 
-        dividend-=divisor
-        counter+=1
-    if store_dividend * store_divisor <0: return (counter-2*counter)
-    else: return counter
-print(divide(1000000000, -3))
+print("hello world!")
