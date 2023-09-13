8ffb083b2ca1a2dd716852a8c73aa923bc897522
Messef <85040583+Messef@users.noreply.github.com>
2023-09-13 17:08:11 -0400

    New files from the mac

---
+++ Python_Programs/macfile.py
@@ -0,0 +1,22 @@
+def factors(num, num2,target):
+    num*=num2
+    x= -abs(num//2)
+    stack=[num, -num]
+    hashmap={}
+    while x <=num//2:
+        if x==0: 
+            x+=1
+            continue
+
+        if num%x==0:
+            stack.append(x)
+        x+=1
+    for x,y in enumerate(stack):
+        hashmap[y] = x
+    for x,y in enumerate(stack):
+        complement=target - y
+        if complement in hashmap.keys()and hashmap[complement] != x:
+            if stack[x]*(target-stack[x])==num: 
+                return stack[x], (target-stack[x])
+    return "none"
+print(factors(6, 5, 16))
