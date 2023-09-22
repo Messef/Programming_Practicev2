dd49c9b5fc356926c900d7ba439dd9f179a0c04c
Messef <85040583+Messef@users.noreply.github.com>
2023-09-22 17:44:58 -0400

    Update main.cpp

--- C++_Programs/main.cpp
+++ C++_Programs/main.cpp
@@ -1,12 +1,18 @@
+// C++ program to initialize
+// a vector from an array.
 #include <iostream>
-#include <string>
+#include <vector>
+using namespace std;
 
-using namespace std; 
+int main()
+{
+	int arr[] = { 10, 20, 30 };
+	int n = sizeof(arr) / sizeof(arr[0]);
 
+	vector<int> vect(arr, arr + n);
 
-int x = 0;
-string y = "";
-int main() {
-    cout<<"Hello World! " << y;
-    return 0;
+	for (int x : vect)
+		cout << x << " " << vect.size() << " " << endl;
+
+	return 0;
 }
