413e4b0c6cecad176c80d3237d155bf9d0002300
Messef <85040583+Messef@users.noreply.github.com>
2023-10-08 23:37:21 -0400

    moved files to be more organized

--- Java_Programs/app.java
+++ Java_Programs/app.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class app {
     static Scanner myScan = new Scanner(System.in);
     public static void main(String[] args) {
-        double x = (int) (5.5 - 2.5);
         
         double y = (int) 5.5 - (int) 2.5;
         
---
+++ Python_Programs/__pycache__/numbers.py
@@ -0,0 +1,12 @@
+def numbertoString(number): 
+    string = ""
+    naming_conventions = {
+        
+    }
+    list_number = [int(x) for x in str(number)]
+    for y, z in enumerate(list_number): 
+        pass
+    print(list_number)
+numbertoString(20)
+
+
