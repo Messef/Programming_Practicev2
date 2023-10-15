88668e4d61b84292dcc78352d69ece3ea648eee0
Messef <85040583+Messef@users.noreply.github.com>
2023-10-14 23:17:54 -0400

    Finished Wordle

--- Python_Programs/__pycache__/codecademy.py
+++ Python_Programs/__pycache__/codecademy.py
@@ -14,48 +14,40 @@ for i in range(6):
     counterhashmap = {
 
     }
-    marker = []
     guess=input("What is your guess(5 letters)? ")
+    if len(guess)!=5: 
+        break
     guess = guess.lower()
     confirmation = []
     msg=""
+    for x in guess: 
+        if x not in counterhashmap:
+            counterhashmap[x]=1
+            continue
+        counterhashmap[x]+=1
     for x,y in enumerate(guess): 
         if y not in theWord: 
             confirmation.append("n")
             continue
         elif y in theWord and guess[x]!=theWord[x]:
-            if y not in counterhashmap:
-                counterhashmap[y]=1
-                marker.append(x)
-                continue
-            counterhashmap[y]+=1
-            if counterhashmap[y]>hashmap[y]:
+            if counterhashmap[y] > hashmap[y]:
                 confirmation.append("n")
                 continue
-            marker.append(x)
+            confirmation.append("m")
+
             continue
         elif guess[x]==theWord[x]:
             if y not in counterhashmap:
-                counterhashmap[y]=1
                 confirmation.append("y")
                 continue
-            counterhashmap[y]+=1
             confirmation.append("y")
-            continue
-    if len(marker)>0:
-        for x in marker: 
-            conf=marker[x]
-            print(x, marker, conf)
-            if counterhashmap[guess[conf]] > hashmap[guess[conf]]:
-                confirmation.insert(conf, "n")
-                continue
-            confirmation.insert(conf, "m")
+
     for x in confirmation:
         msg+=x
     print(msg)
     if msg == "yyyyy":
         break
-
+print(theWord)
 
 
 
