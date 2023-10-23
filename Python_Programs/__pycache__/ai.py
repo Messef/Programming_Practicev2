5a6b738149ac5feae37f4d3678dee0824eb7731c
Messef <85040583+Messef@users.noreply.github.com>
2023-10-22 22:45:28 -0400

    Make a basic entity that can play chess

--- Python_Programs/__pycache__/ai.py
+++ Python_Programs/__pycache__/ai.py
@@ -1,144 +1,68 @@
-import sys
+myMove = input = ("piece and move:")
+counter = 0
+files = {
+    1: 'a',
+    2: 'b',
+    3: 'c',
+    4: 'd',
+    5: 'e',
+    6: 'f',
+    7: 'g',
+    8: 'h'
 
-class Node():
-    def __init__(self, state, pareent, action):
-        self.state = state
-        self.pareent = pareent
-        self.action = action
+}
+initial_state = [
+        ['r1w', 'nw', 'bw', 'qw', 'kw', 'bw', 'nw', 'r2w'],
+        ['pw', 'pw', 'pw', 'pw', 'pw', 'pw', 'pw', 'pw'],
+        ['e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'],
+        ['e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'],
+        ['e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'],
+        ['e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'],
+        ['pb', 'pb', 'pb', 'pb', 'pb', 'pb', 'pb', 'pb'],
+        ['r1b', 'nb', 'bb', 'qb', 'kb', 'bb', 'nb', 'r2b']
+        ]
+state = [
+        ['rw', 'nw', 'bw', 'qw', 'kw', 'bw', 'nw', 'rw'],
+        ['pw', 'pw', 'pw', 'pw', 'pw', 'pw', 'pw', 'pw'],
+        ['e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'],
+        ['e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'],
+        ['e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'],
+        ['e', 'e', 'e', 'e', 'e', 'e', 'e', 'e'],
+        ['pb', 'pb', 'pb', 'pb', 'pb', 'pb', 'pb', 'pb'],
+        ['rb', 'nb', 'bb', 'qb', 'kb', 'bb', 'nb', 'rb']
+        ]
+class Board:
+    def __init__(self, initial_state, state):
+        self.initial_state = initial_state
+        self.state = state 
+    def get_state(self):
+        return self.initial_state
+    def coordState(self):
+        pass
+    
+myBoard = Board(initial_state, state)
+class King: 
+    def __init__(self, color, coordinate, rawcoordinate, wantMove):
+        self.color = color
+        self.coord = coordinate
+        self.rawCoord = rawcoordinate
+        self.wantMove = wantMove
+    def get_coord(self):
+        myCoord = f"k{self.color}"
+        print(myCoord)
+        for x, y in enumerate(myBoard.state):
+            print(y)
+            for z, a in enumerate(y):
+                if myBoard.state[x][z] == myCoord:
+                    self.coord = files[z+1]+str(x+1)
+                    self.rawCoord = [z+1, x+1]
+                    break
+        return self.coord
+    def valid_move(self):
+        pass
+                   
 
-class StackFrontier():
-    def __init__(self): 
-        self.frontier = []
 
-    def add(self, node):
-        self.frontier.append(node)
-    def contains_state(self, state):
-        return any(node.state == state for node in self.frontier)
-    def pop(self):
-        return self.frontier.pop()
-    def empty(self):
-        return len(self.frontier) == 0
-    def remove(self):
-        if self.empty():
-            raise Exception("Stack is empty")
-        else:
-            node = self.frontier[-1]
-            self.frontier = self.frontier[:-1]
-            return node
+myKing = King("w", "no coord found", "no coord found", "e2")
 
-class QueueFrontier(StackFrontier):
-    def remove(self):
-        if self.empty():
-            raise Exception("Queue is empty")
-        else:
-            node = self.frontier[0]
-            self.frontier = self.frontier[1:]
-            return node
-
-class Maze():
-
-    def __init__(self, filename):
-        with open(filename) as f:
-            contents = f.read()
-        
-        if contents.count("A") != 1:
-            raise Exception("Invalid maze file")
-        if contents.count("B")!= 1:
-            raise Exception("Invalid maze file")
-        
-        contents = contents.splitlines()
-        self.height = len(contents)
-        self.width = max(len(line) for line in contents)
-
-        self.walls = []
-        for i in range(self.height):
-            row = []
-            for j in range(self.width):
-                try:
-                    if contents[i][j] == "A":
-                        self.start = (i, j)
-                        row.append(False)
-                    elif contents[i][j] == "B":
-                        self.goal = (i, j)
-                        row.append(False)
-                    elif contents[i][j] == " ":
-                        row.append(False)
-                    else: 
-                        row.append(True)
-                except IndexError:
-                    row.append(False)
-            self.walls.append(row)
-        self.solution = None
-    def print(self):
-        solution = self.solution[1] if self.solution is not None else None
-        print()
-        for i,row in enumerate(self.walls):
-            for j, col in enumerate(row):
-                if col:
-                    print(" ", end="")
-                elif (i, j) == self.start:
-                    print("A", end="")
-                elif (i, j) == self.goal:
-                    print("B", end="")
-                elif solution is not None and (i, j) == solution:
-                    print("*", end="")
-                else:
-                    print(" ", end="")
-            print()
-        print()
-
-        def neighbors(self, state):
-            row, col = state
-
-            candidates = [
-                ("up", (row - 1, col)),
-                ("right", (row, col + 1)),
-                ("down", (row + 1, col)),
-                ("left", (row, col - 1))
-            ]
-            result = []
-            for action, (r, c) in candidates:
-                try:
-                    if not self.walls[r][c]:
-                        result.append((action, (r, c)))
-                except IndexError:
-                    continue
-            return result
-        
-def solve(self):
-    """Solve the maze"""
-
-    #Initialize frontier to just the starting position
-    self.num_explored = 0
-    start = Node(state = self.start, parent = None, action = None)
-    frontier = StackFrontier()
-    frontier.add(start)
-
-    # initialize an empty explored set 
-    self.explored = set()
-
-    #Keep looping until the goal is reached
-    while True:
-        if frontier.empty():
-            raise Exception("No solution found")
-        
-        node = frontier.remove()
-
-        if node.state == self.goal:
-            actions = []
-            cells = []
-            while node.parent is not None:
-                actions.append(node.action)
-                cells.append(node.state)
-                node = node.parent
-            actions.reverse()
-            cells.reverse()
-            self.solution = (actions, cells)
-            return
-
-        self.explored.add(node.state)
-
-        for action, state in self.neighbors(node.state):
-            if not frontier.contains_state(state) and state not in self.explored:
-                child = Node(state=state, parent=node, action=action)
-                frontier.add(child)
+print(myKing.get_coord())
