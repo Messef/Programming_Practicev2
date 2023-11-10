b6309e378df17c4a7891cbd2074085ec8bd1b319
Messef <85040583+Messef@users.noreply.github.com>
2023-11-10 11:11:21 -0500

    updated cpp

--- C++_Programs/example.cpp
+++ C++_Programs/example.cpp
@@ -1,18 +1,42 @@
 #include <iostream>
-#include <cmath>
 
 int main() {
-  double a;
-  double b;
-  double c;
-  std::cout<<"a: ";
-  std::cin>> a;
-  std::cout<<"b: ";
-  std::cin>> b;
-  std::cout<<"c: ";
-  std::cin>> c;
-  double root2 = (-b - std::sqrt(b**2-4*a*c))/(2*a);;
-  double root1 = (-b + std::sqrt(b**2-4*a*c))/(2*a);
-  std::cout << "root 1 is " << root1 <<std::endl;
-  std::cout << "root 2 is " << root2 <<std::endl;
+  
+  
+
+  int planetNum;
+  std::cout << "Planet Num? ";
+  std::cin>>planetNum;
+  switch(planetNum) {
+    case 1 :
+      std::cout<<"relative grav: .38\n";
+      break;
+    case 2 :
+      std::cout<<"relative grav: .91\n";
+      break;
+    case 3 :
+      std::cout<<"relative grav: .38\n";
+      break;
+    case 4 :
+      std::cout<<"relative grav: 2.34\n";
+      break;
+    case 5 :
+      std::cout<<"relative grav: 1.06\n";
+      break;
+    case 6 :
+      std::cout<<"relative grav: .92\n";
+      break;
+    case 7 :
+      std::cout<<"relative grav: 1.19\n";
+      break;
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
 }
