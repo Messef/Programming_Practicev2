0982e198e89dcceac1c0ddfb40908ebfa0cf889b
Messef <85040583+Messef@users.noreply.github.com>
2023-11-10 10:12:05 -0500

    updated cpp

--- C++_Programs/example.cpp
+++ C++_Programs/example.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
+#include <cmath>
 
 int main() {
-  // Add your code below  
-  double weight;
- std::cout<<"weight of an item? " <<std::endl;
-std::cin>>weight;
-double dist;
- std::cout<<"distance in miles? " <<std::endl;
-std::cin>>dist;
-std::cout << "obj would weight: " << .38 * weight << std::endl;
-std::cout << "miles to km: " << dist * 1.609 << std::endl;
-return 0;
+  double a;
+  double b;
+  double c;
+  std::cout<<"a: ";
+  std::cin>> a;
+  std::cout<<"b: ";
+  std::cin>> b;
+  std::cout<<"c: ";
+  std::cin>> c;
+  double root2 = (-b - std::sqrt(b**2-4*a*c))/(2*a);;
+  double root1 = (-b + std::sqrt(b**2-4*a*c))/(2*a);
+  std::cout << "root 1 is " << root1 <<std::endl;
+  std::cout << "root 2 is " << root2 <<std::endl;
 }
