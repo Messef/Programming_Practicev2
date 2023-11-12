d429edc6635807780d2cdfda1f367ae645d0556b
Messef <85040583+Messef@users.noreply.github.com>
2023-11-11 20:14:41 -0500

    updated cpp with codecademy challenges

--- C++_Programs/example.cpp
+++ C++_Programs/example.cpp
@@ -1,42 +1,26 @@
 #include <iostream>
+#include <vector>
 
 int main() {
-  
-  
+  std::vector<int> challenge;
+  challenge.push_back(2);
+  challenge.push_back(4);
+  challenge.push_back(3);
+  challenge.push_back(6);
+  challenge.push_back(1);
+  challenge.push_back(9);
+  int even;
+  int odd = 1;
+  for (int i = 0; i<challenge.size(); i++) {
+    if (challenge[i]%2==0) {
+      even+=challenge[i];
+    } else {
 
-  int planetNum;
-  std::cout << "Planet Num? ";
-  std::cin>>planetNum;
-  switch(planetNum) {
-    case 1 :
-      std::cout<<"relative grav: .38\n";
-      break;
-    case 2 :
-      std::cout<<"relative grav: .91\n";
-      break;
-    case 3 :
-      std::cout<<"relative grav: .38\n";
-      break;
-    case 4 :
-      std::cout<<"relative grav: 2.34\n";
-      break;
-    case 5 :
-      std::cout<<"relative grav: 1.06\n";
-      break;
-    case 6 :
-      std::cout<<"relative grav: .92\n";
-      break;
-    case 7 :
-      std::cout<<"relative grav: 1.19\n";
-      break;
+      odd*=challenge[i];
+    }
   }
-  
-  
-  
-  
-  
-  
-  
-  
-  
+  std:: cout<< "Sum of all even nums: " << even << std::endl;
+  std:: cout<< "Product of all odd nums: " << odd << std::endl;
+  return 0;
+
 }
