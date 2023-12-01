84a7a59bbde60cc26547fcd37beb4e7a477db4eb
Messef <85040583+Messef@users.noreply.github.com>
2023-12-01 15:20:32 -0500

    Wanted to check compsci answers on my own machine

--- Java_Programs/CodeChecker.java
+++ Java_Programs/CodeChecker.java
@@ -1,14 +1,16 @@
 package Java_Programs;
 
 public class CodeChecker   {
-    public static void main (String [] args){
-        int max = -5;
-        int min = -7;
-        for (int i = 0; i < 30; i++) {
-            double rn = Math.random();
-            int num = (int)(rn * (max - min+1)) + min;
-            System.out.println(num);
-        }
+    private double x;
+    public CodeChecker(double avacadoWeight) {
+        x = avacadoWeight;
+    }
+    public double avacadoWeight() {
+        return x;
+    }
+    public int getBox(double weightBox, double tolerance) {
+        double max = weightBox - tolerance;
+        return (int)Math.floor(max /  avacadoWeight());
         
     }
 }
---
+++ Java_Programs/Runner.java
@@ -0,0 +1,14 @@
+package Java_Programs;
+/**
+ * Write a description of class StringChallenge here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Runner
+{
+    public static void main(String[] args) {
+    CodeChecker cChecker = new CodeChecker(5.7);
+    System.out.println(cChecker.getBox(50, 5));
+   }
+}
