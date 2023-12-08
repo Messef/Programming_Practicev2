f67088e8fa67bf6e8444f624d99f4091c3f7b254
Messef <85040583+Messef@users.noreply.github.com>
2023-12-08 14:30:23 -0500

    autoboxing practice

--- Java_Programs/Runner.java
+++ Java_Programs/Runner.java
@@ -7,8 +7,17 @@ package Java_Programs;
  */
 public class Runner
 {
-    public static void main(String[] args) {
+   /* public static void main(String[] args) {
     CodeChecker cChecker = new CodeChecker(5.7);
     System.out.println(cChecker.getBox(50, 5));
-   }
+    */
+    public static void main(String[] args) {
+        Integer x = new Integer(5);
+        Integer y = new Integer();
+        System.out.println(x.intValue());
+
 }
+
+//System.out.print(3 * x);
+    }
+   
