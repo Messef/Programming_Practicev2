737614158f9fcb57504c63c5fb65714fca2d6c0e
Messef <85040583+Messef@users.noreply.github.com>
2023-12-26 12:15:14 -0500

    Been a while

--- C++_Programs/leetcode.cpp
+++ C++_Programs/leetcode.cpp
@@ -1,43 +1,27 @@
-#include <map>
 #include <iostream>
 #include <vector>
 class Solution {
 public:
-    std::vector<int> twoSum(std::vector<int>& nums, int target) {
-        std::map<int, int> numMap;
-        int n = nums.size();
-
-        // Build the hash table
-        for (int i = 0; i < n; i++) {
-            numMap[nums[i]] = i;
+    std::vector<int> plusOne(std::vector<int>& digits) {
+        
+        digits[-1]++;
+        if (int(log10(digits[-1]) + 1 == 2)) {
+            digits.push_back(0);
+            digits[-2] = 1; 
         }
-
-        std::vector<int> returner;
-        // Find the complement
-        for (int i = 0; i < n; i++) {
-            int complement = target - nums[i];
-            if (numMap.count(complement) && numMap[complement] != i) {
-                returner.push_back(numMap[complement]);
-                returner.push_back(i);
-                return returner;
-            }
-        }
-
-        return returner; // No solution found
+        return digits;
     }
 };
 int main() {
-    std::vector<int> solution;
-    for (int i = 0; i < 10; i++) {
-        solution.push_back(i);
-    }
-    int target = 17;
     Solution myObj;
-    std::vector<int> result = myObj.twoSum(solution, target);
-    
-    // Print the result
-    for (int num : result) {
-        std::cout << num << std::endl;
+    std::vector<int> digits;
+    digits.push_back(3);
+    digits.push_back(4);
+    digits.push_back(9);
+
+    std::vector<int> ans = myObj.plusOne(digits);
+    for (int i: ans) {
+        std::cout<<i;
     }
     
     return 0;
